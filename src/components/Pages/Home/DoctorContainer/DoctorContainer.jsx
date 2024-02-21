6f902ebc8b2b195582aed50e9e94248c30ceafd9
RidoyMia <ridoymia153159hero@gmail.com>
2024-02-21 00:40:01 -0800

    banner completed

---
+++ src/components/Pages/Home/DoctorContainer/DoctorContainer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import doctor from "../../../../imgages/Doctor.png"
+import teeth from "../../../../imgages/teethImage.png"
+import Image from 'next/image';
+const DoctorContainer = () => {
+    return (
+        <div className='px-5 my-16'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-x-10 gap-y-20 md:grid-cols-2 lg:px-20 md:px-16 xl:px-28'>
+                <div>
+                    <Image src={doctor} className='w-full h-[800px]' width={100} height={600}></Image>
+                </div>
+                <div>
+                    <h1 className='text-3xl font-semibold'>Our Services</h1>
+                    <p className='py-5 font-[10px]'>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                      <div className='flex justify-between items-center align-middle my-5'>
+                        <button className='py-3 lg:px-5 md:px-1 px-2 bg-[#F7A582] text-white rounded-md text-[10px] xl:text-sm'>Cavity Protection</button>
+                        <button className='py-3 lg:px-5 md:px-1 px-2 bg-[#F7A582] text-white rounded-md text-[10px] xl:text-sm'>Cavity Protection</button>
+                        <button className='py-3 lg:px-5 md:px-1 px-2 bg-[#F7A582] text-white rounded-md text-[10px] xl:text-sm'>Cavity Protection</button>
+                      </div>
+                    <Image src={teeth} className='w-full h-[300px] my-5' width={200} height={500}></Image>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error </p>
+                    <p className='pt-3'>Sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inve ntore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                    <button className='py-3 px-7 rounded-md border my-5 border-red-200 hover:bg-[#F7A582] hover:text-white'>More Details</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DoctorContainer;
---
+++ src/components/Shared/Header/Header.jsx
@@ -0,0 +1,41 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100   shadow-xl">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li><Link className='text-lg' href="/">Home</Link></li>
+        <li><Link className='text-lg' href="/service">Services</Link></li>
+        <li><Link className='text-lg' href="/contact">Contact us</Link></li>
+   
+        
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+        <Link className='text-lg  px-5 font-[500] ' href="/">Home</Link>
+        <Link className='text-lg px-5 font-[500]' href="/service">Services</Link>
+       <Link className='text-lg px-5 font-[500]' href="/contact">Contact us</Link>
+      
+    </ul>
+  </div>
+  <div className="navbar-end">
+  <ul className="menu menu-horizontal px-1">
+  <li><Link className='text-lg' href="/login">Login</Link></li>
+  </ul>
+  </div>
+</div>
+        </div>
+    );
+};
+
+export default Header;
