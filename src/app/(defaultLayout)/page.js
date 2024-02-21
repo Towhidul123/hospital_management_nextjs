60a3a255f7ba91ef1846588fafa0e3738a50dc5e
RidoyMia <ridoymia153159hero@gmail.com>
2024-02-21 01:32:49 -0800

    doctor section completed

--- src/app/(defaultLayout)/page.js
+++ src/app/(defaultLayout)/page.js
@@ -1,10 +1,12 @@
 import Banner from '@/components/Pages/Home/Banner/Banner';
+import DoctorContainer from '@/components/Pages/Home/DoctorContainer/DoctorContainer';
 import React from 'react';
 
 function page(props) {
   return (
     <div>
       <Banner></Banner>
+      <DoctorContainer></DoctorContainer>
     </div>
   );
 }
