fdbb8e796c83cd737a5c9b54543b377b2a0a2339
Towhidul123 <excaliburfsh@gmail.com>
2024-02-23 03:42:33 +0600

    shadcn

--- src/app/(defaultLayout)/layout.js
+++ src/app/(defaultLayout)/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "../../components/Shared/Header/Header";
+import Footer from "../../components/Pages/Footer/Footer";
 import { ClerkProvider } from "@clerk/nextjs";
 import Nav from "./components/nav";
 
@@ -19,7 +20,7 @@ export default function RootLayout({ children }) {
           <Header></Header>
 
           {children}
-
+          <Footer></Footer>
         </body>
       </html>
     </ClerkProvider>
